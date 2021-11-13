3501256d5e074ca88879b8cf8d26e38d5f4651b0
tejas200713 <82149946+tejas200713@users.noreply.github.com>
2021-11-13 12:54:56 +0530

    Add files via upload

---
+++ C37RV_SpeedRacer_StudentActivity-main/js/Game.js
@@ -0,0 +1,52 @@
+class Game {
+
+  constructor() {}
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+    car1 = createSprite(width/2-30,height-100);
+    car1.addImage(car1_img);
+    car1.scale=0.05;
+    car2=createSprite(width/2+170,height-100);
+    car2.addImage(car2_img);
+    car2.scale = 0.05;
+     cars = [car1,car2];
+     car3 = createSprite(width/2-200,height-100);
+     car3.addImage(car1_img);
+     car3.scale=0.05;
+
+  }
+
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+  }
+
+  play() {
+    this.handleElements();
+    Player.getPlayersInfo();
+    if(allPlayers!==undefined){
+      image (track,0,-height*5,width,height*6);
+
+    }
+    drawSprites();
+  }
+  update(state){
+    database.ref('/').update({
+      gameState:state
+    })
+  }
+
+}
